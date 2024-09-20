6c5a928f4291bfedd038e5fed0e5ab301c9d9bb0
Ethanthegrand <151371267+Ethanthegrand@users.noreply.github.com>
2024-09-20 13:35:23 +1000

    Add files via upload

--- FastCanvas.lua
+++ FastCanvas.lua
@@ -6,21 +6,18 @@
 	
 	This module was designed to be intergrated with CanvasDraw. 
 	A real-time roblox pixel graphics engine.
-	
-	This can be used on normal GUI Frames AND Decals, Textures, MeshParts, etc
 
 	Written by @Ethanthegrand14
 	
 	Created: 9/11/2023
-	Last Updated: 27/04/2024
+	Last Updated: 30/12/2023
 ]]
 
 local FastCanvas = {}
 
-type ParentType = GuiObject | Decal | Texture | SurfaceAppearance | MeshPart
+local Allowed
 
-function FastCanvas.new(Width: number, Height: number, CanvasParent: ParentType, Blur: boolean?)
-	local IsUiParent = CanvasParent:IsA("GuiObject")
+function FastCanvas.new(Width: number, Height: number, CanvasParent: GuiObject, Blur: boolean?)
 	
 	local Canvas = {} -- The canvas object
 	local Grid = table.create(Width * Height * 4, 1) -- Local pixel grid containing RGBA values
@@ -35,40 +32,33 @@ function FastCanvas.new(Width: number, Height: number, CanvasParent: ParentType,
 	
 	-- Create gui objects
 	
+	local CanvasFrame = Instance.new("ImageLabel")
+	CanvasFrame.Name = "FastCanvas"
+	CanvasFrame.BackgroundTransparency = 1
+	CanvasFrame.ClipsDescendants = true
+	CanvasFrame.Size = UDim2.fromScale(1, 1)
+	CanvasFrame.Position = UDim2.fromScale(0.5, 0.5)
+	CanvasFrame.AnchorPoint = Vector2.new(0.5, 0.5)
+	
+	if not Blur then
+		CanvasFrame.ResampleMode = Enum.ResamplerMode.Pixelated
+	end
+
+	local AspectRatio = Instance.new("UIAspectRatioConstraint")
+	AspectRatio.AspectRatio = Width / Height
+	AspectRatio.Parent = CanvasFrame
+	
 	local EditableImage = Instance.new("EditableImage")
 	EditableImage.Size = Resolution
+	EditableImage.Parent = CanvasFrame
 	
-	local CanvasFrame
+	CanvasFrame.Parent = CanvasParent
 	
-	if IsUiParent then
-		CanvasFrame = Instance.new("ImageLabel")
-		CanvasFrame.Name = "FastCanvas"
-		CanvasFrame.BackgroundTransparency = 1
-		CanvasFrame.ClipsDescendants = true
-		CanvasFrame.Size = UDim2.fromScale(1, 1)
-		CanvasFrame.Position = UDim2.fromScale(0.5, 0.5)
-		CanvasFrame.AnchorPoint = Vector2.new(0.5, 0.5)
-		
-		if not Blur then
-			CanvasFrame.ResampleMode = Enum.ResamplerMode.Pixelated
-		end
-		
-		local AspectRatio = Instance.new("UIAspectRatioConstraint")
-		AspectRatio.AspectRatio = Width / Height
-		AspectRatio.Parent = CanvasFrame
-		
-		EditableImage.Parent = CanvasFrame
-		CanvasFrame.Parent = CanvasParent
-	else
-		EditableImage.Parent = CanvasParent
-	end
-	
-	-- Properties
+	-- Canvas properties
 	
 	Canvas.Image = EditableImage
 	
-	
-	-- Pixel methods
+	-- Pixel set methods
 	
 	function Canvas:SetColor3(X: number, Y: number, Colour: Color3)
 		local Index = GetGridIndex(X, Y)
@@ -84,36 +74,22 @@ function FastCanvas.new(Width: number, Height: number, CanvasParent: ParentType,
 		Grid[Index + 2] = B
 	end
 	
-	function Canvas:SetRGBA(X: number, Y: number, R: number, G: number, B: number, A: number)
-		local Index = GetGridIndex(X, Y)
-		Grid[Index] = R
-		Grid[Index + 1] = G
-		Grid[Index + 2] = B
-		Grid[Index + 3] = A
-	end
-	
 	function Canvas:SetAlpha(X: number, Y: number, Alpha: number)
 		Grid[GetGridIndex(X, Y) + 3] = Alpha
 	end
 	
 	-- Pixel fetch methods
 	
-	function Canvas:GetRGB(X: number, Y: number): (number, number, number)
-		local Index = GetGridIndex(X, Y)
-
-		return Grid[Index], Grid[Index + 1], Grid[Index + 2]
-	end
-
-	function Canvas:GetRGBA(X: number, Y: number): (number, number, number, number)
-		local Index = GetGridIndex(X, Y)
-
-		return Grid[Index], Grid[Index + 1], Grid[Index + 2], Grid[Index + 3]
-	end
-	
 	function Canvas:GetColor3(X: number, Y: number): Color3
 		local Index = GetGridIndex(X, Y)
 		
 		return Color3.new(Grid[Index], Grid[Index + 1], Grid[Index + 2])
+	end
+	
+	function Canvas:GetRGB(X: number, Y: number): (number, number, number)
+		local Index = GetGridIndex(X, Y)
+		
+		return Grid[Index], Grid[Index + 1], Grid[Index + 2]
 	end
 	
 	function Canvas:GetAlpha(X: number, Y: number): number
@@ -124,24 +100,14 @@ function FastCanvas.new(Width: number, Height: number, CanvasParent: ParentType,
 	
 	-- Canvas methods
 	
-	function Canvas:SetGrid(PixelArray)
-		Grid = table.clone(PixelArray)
-	end
-
-	function Canvas:GetGrid()
-		return table.clone(Grid)
-	end
-	
 	function Canvas:Render()
 		EditableImage:WritePixels(Origin, Resolution, Grid)
 	end
 	
 	function Canvas:Destroy()
-		if CanvasFrame then
-			CanvasFrame:Destroy()
-		end
-		Grid = nil
 		Canvas = nil
+		Grid = nil
+		CanvasFrame:Destroy()
 	end
 	
 	return Canvas
