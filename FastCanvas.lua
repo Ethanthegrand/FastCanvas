26fff9f1c3048a057f31f6b24b17c3513c83a894
Ethanthegrand <151371267+Ethanthegrand@users.noreply.github.com>
2024-09-28 10:27:43 +1000

    Add files via upload

--- FastCanvas.lua
+++ FastCanvas.lua
@@ -23,17 +23,40 @@ function FastCanvas.new(Width: number, Height: number, CanvasParent: ParentType,
 	local IsUiParent = CanvasParent:IsA("GuiObject")
 	
 	local Canvas = {} -- The canvas object
-	local Grid = table.create(Width * Height * 4, 1) -- Local pixel grid containing RGBA values
-	local CurrentClearRGBA = {1, 1, 1, 1}
-	local ClearingGrid = table.clone(Grid) -- For Canvas:Clear()
+	local Grid = buffer.create(Width * Height * 4) -- Local pixel grid containing RGBA values
+	local CurrentClearRGBA = buffer.create(4)
+	local ClearingGrid = buffer.create(Width * Height * 4) -- For Canvas:Clear()
 	
 	local Origin = Vector2.zero
 	local Resolution = Vector2.new(Width, Height)
 	
 	-- Local functions
 	local function GetGridIndex(X, Y)
-		return (X + (Y - 1) * Width) * 4 - 3
+		return (X + (Y - 1) * Width) * 4 - 4
 	end
+	
+	-- Initialise buffers
+	
+	local Index = 0
+	for _ = 1, Width * Height do
+		
+		buffer.writeu8(Grid, Index, 255)
+		buffer.writeu8(Grid, Index + 1, 255)
+		buffer.writeu8(Grid, Index + 2, 255)
+		buffer.writeu8(Grid, Index + 3, 255)
+		
+		buffer.writeu8(ClearingGrid, Index, 255)
+		buffer.writeu8(ClearingGrid, Index + 1, 255)
+		buffer.writeu8(ClearingGrid, Index + 2, 255)
+		buffer.writeu8(ClearingGrid, Index + 3, 255)
+		
+		Index += 4
+	end
+	
+	buffer.writeu8(CurrentClearRGBA, 0, 255)
+	buffer.writeu8(CurrentClearRGBA, 1, 255)
+	buffer.writeu8(CurrentClearRGBA, 2, 255)
+	buffer.writeu8(CurrentClearRGBA, 3, 255)
 	
 	-- Create gui objects
 	
@@ -75,28 +98,28 @@ function FastCanvas.new(Width: number, Height: number, CanvasParent: ParentType,
 	
 	function Canvas:SetColor3(X: number, Y: number, Colour: Color3)
 		local Index = GetGridIndex(X, Y)
-		Grid[Index] = Colour.R
-		Grid[Index + 1] = Colour.G
-		Grid[Index + 2] = Colour.B
+		buffer.writeu8(Grid, Index, Colour.R * 255)
+		buffer.writeu8(Grid, Index + 1, Colour.G * 255)
+		buffer.writeu8(Grid, Index + 2, Colour.B * 255)
 	end
 	
 	function Canvas:SetRGB(X: number, Y: number, R: number, G: number, B: number)
 		local Index = GetGridIndex(X, Y)
-		Grid[Index] = R
-		Grid[Index + 1] = G
-		Grid[Index + 2] = B
+		buffer.writeu8(Grid, Index, R * 255)
+		buffer.writeu8(Grid, Index + 1, G * 255)
+		buffer.writeu8(Grid, Index + 2, B * 255)
 	end
 	
 	function Canvas:SetRGBA(X: number, Y: number, R: number, G: number, B: number, A: number)
 		local Index = GetGridIndex(X, Y)
-		Grid[Index] = R
-		Grid[Index + 1] = G
-		Grid[Index + 2] = B
-		Grid[Index + 3] = A
+		buffer.writeu8(Grid, Index, R * 255)
+		buffer.writeu8(Grid, Index + 1, G * 255)
+		buffer.writeu8(Grid, Index + 2, B * 255)
+		buffer.writeu8(Grid, Index + 3, A * 255)
 	end
 	
 	function Canvas:SetAlpha(X: number, Y: number, Alpha: number)
-		Grid[GetGridIndex(X, Y) + 3] = Alpha
+		buffer.writeu8(Grid, GetGridIndex(X, Y) + 3, Alpha * 255)
 	end
 	
 	-- Pixel fetch methods
@@ -104,57 +127,64 @@ function FastCanvas.new(Width: number, Height: number, CanvasParent: ParentType,
 	function Canvas:GetRGB(X: number, Y: number): (number, number, number)
 		local Index = GetGridIndex(X, Y)
 
-		return Grid[Index], Grid[Index + 1], Grid[Index + 2]
+		return buffer.readu8(Grid, Index) / 255, buffer.readu8(Grid, Index + 1) / 255, buffer.readu8(Grid, Index + 2) / 255
 	end
 
 	function Canvas:GetRGBA(X: number, Y: number): (number, number, number, number)
 		local Index = GetGridIndex(X, Y)
 
-		return Grid[Index], Grid[Index + 1], Grid[Index + 2], Grid[Index + 3]
+		return buffer.readu8(Grid, Index) / 255, buffer.readu8(Grid, Index + 1) / 255, buffer.readu8(Grid, Index + 2) / 255, buffer.readu8(Grid, Index + 3) / 255
 	end
 	
 	function Canvas:GetColor3(X: number, Y: number): Color3
 		local Index = GetGridIndex(X, Y)
 		
-		return Color3.new(Grid[Index], Grid[Index + 1], Grid[Index + 2])
+		return Color3.new(buffer.readu8(Grid, Index) / 255, buffer.readu8(Grid, Index + 1) / 255, buffer.readu8(Grid, Index + 2) / 255)
 	end
 	
 	function Canvas:GetAlpha(X: number, Y: number): number
 		local Index = GetGridIndex(X, Y)
 
-		return Grid[Index + 3]
+		return buffer.readu8(Grid, Index + 3) / 255
 	end
 	
 	-- Canvas methods
 	
 	function Canvas:SetGrid(PixelArray)
-		Grid = table.clone(PixelArray)
+		for i, Value in pairs(PixelArray) do
+			buffer.writeu8(Grid, i - 1, Value * 255)
+		end
 	end
 
 	function Canvas:GetGrid()
-		return table.clone(Grid)
+		local ReturnArray = {}
+		
+		for i = 1, Width * Height * 4 do
+			ReturnArray[i] = buffer.readu8(Grid, i - 1) / 255
+		end
+		
+		return ReturnArray
 	end
 	
 	function Canvas:SetClearRGBA(R, G, B, A)
 		for i = 1, Width * Height * 4, 4 do
-			ClearingGrid[i] = R
-			ClearingGrid[i + 1] = G
-			ClearingGrid[i + 2] = B
-			ClearingGrid[i + 3] = A
+			i -= 1
+			buffer.writeu8(ClearingGrid, i, R * 255)
+			buffer.writeu8(ClearingGrid, i + 1, G * 255)
+			buffer.writeu8(ClearingGrid, i + 2, B * 255)
+			buffer.writeu8(ClearingGrid, i + 3, A * 255)
 		end
 	end
 	
 	function Canvas:Clear()
-		Grid = table.clone(ClearingGrid)
+		buffer.copy(Grid, 0, ClearingGrid, 0)
 	end
 	
 	function Canvas:Render()
-		EditableImage:WritePixels(Origin, Resolution, Grid)
+		EditableImage:WritePixelsBuffer(Origin, Resolution, Grid)
 	end
 
-	function Canvas:Resize(NewWidth, NewHeight)
-		self:Clear()
-		
+	function Canvas:Resize(NewWidth, NewHeight)	
 		Width, Height = NewWidth, NewHeight
 		Resolution = Vector2.new(NewWidth, NewHeight)
 		EditableImage.Size = Resolution
@@ -165,10 +195,29 @@ function FastCanvas.new(Width: number, Height: number, CanvasParent: ParentType,
 			AspectRatio.AspectRatio = NewWidth / NewHeight
 		end
 		
-		Grid = table.create(NewWidth * NewHeight * 4, 1)
+		-- Initialise buffers
 		
-		ClearingGrid = table.create(NewWidth * NewHeight * 4)
-		self:SetClearRGBA(table.unpack(CurrentClearRGBA))
+		Grid = buffer.create(NewWidth * NewHeight * 4)
+		ClearingGrid = buffer.create(NewWidth * NewHeight * 4)
+
+		local Index = 0
+		for _ = 1, NewWidth * NewHeight do
+			buffer.writeu8(Grid, Index, 255)
+			buffer.writeu8(Grid, Index + 1, 255)
+			buffer.writeu8(Grid, Index + 2, 255)
+			buffer.writeu8(Grid, Index + 3, 255)
+
+			Index += 4
+		end
+		
+		self:SetClearRGBA(
+			buffer.readu8(CurrentClearRGBA, 0) / 255,
+			buffer.readu8(CurrentClearRGBA, 1) / 255,
+			buffer.readu8(CurrentClearRGBA, 2) / 255,
+			buffer.readu8(CurrentClearRGBA, 3) / 255
+		)
+		
+		self:Clear()
 	end
 	
 	function Canvas:Destroy()
